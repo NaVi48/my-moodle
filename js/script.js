3aa985cacbe4546cfffd2eea2640b005174f76e3
Fbda23-017 kutlwano Mangwala <163797634+NaVi48@users.noreply.github.com>
2025-04-03 02:22:00 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -20,6 +20,27 @@ const jobs = [
         phone: "+1 (555) 456-7890",
         jobDescription: "Cloud Architecture Specialist",
         companyLogo: "https://imgs.search.brave.com/MROBAIODmrKEwcIgpPyzGL9Qlz4cRicSfobW6PFMNOw/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9jZG4u/dmVjdG9yc3RvY2su/Y29tL2kvcHJldmll/dy0xeC81MS82Mi9p/bm5vdmF0aW9uLXRl/Y2hub2xvZ3ktY29t/cGFueS1hYnN0cmFj/dC1sb2dvLXZlY3Rv/ci0xOTUzNTE2Mi5q/cGc"
+    },
+    {
+        companyName: "Tech Innovations Inc.",
+        email: "careers@techinnovations.com",
+        phone: "+1 (555) 123-4567",
+        jobDescription: "Senior Software Engineer - Full Stack Development",
+        companyLogo: "https://imgs.search.brave.com/PPD6j2k8yZ31g_QjvHxFPRIORX1zppwhYZ6fiLzJb00/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9jZG4u/bG9nb2pveS5jb20v/d3AtY29udGVudC91/cGxvYWRzLzIwMTgv/MDUvMzAxNzU1MTQv/MzEwMi03Njh4NTkx/LnBuZw"
+    },
+    {
+        companyName: "Global Solutions Ltd.",
+        email: "jobs@globalsolutions.com",
+        phone: "+1 (555) 987-6543",
+        jobDescription: "Data Scientist - Machine Learning Specialist",
+        companyLogo: "https://imgs.search.brave.com/flG-ELv58N2Izk7zGnaqA2M4FHfNxXrhLXd1vEdLS6M/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly93d3cu/Z2xvYmFsdHMuY29t/L2ltYWdlcy9nbG9i/YWwtbGluay1jb21t/dW5pY2F0aW9ucy1o/b21lcGFnZS1sb2dv/LnBuZws"
+    },
+    {
+        companyName: "Innovative Tech Corp",
+        email: "careers@innovativetech.com",
+        phone: "+1 (555) 456-7890",
+        jobDescription: "Cloud Architecture Specialist",
+        companyLogo: "https://imgs.search.brave.com/MROBAIODmrKEwcIgpPyzGL9Qlz4cRicSfobW6PFMNOw/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9jZG4u/dmVjdG9yc3RvY2su/Y29tL2kvcHJldmll/dy0xeC81MS82Mi9p/bm5vdmF0aW9uLXRl/Y2hub2xvZ3ktY29t/cGFueS1hYnN0cmFj/dC1sb2dvLXZlY3Rv/ci0xOTUzNTE2Mi5q/cGc"
     }
 ];
 
